6a52307d76ec38f047dab90929bb781b5d11614c
AghilesMeghari <aghiles.meghari@soprasteria.com>
2023-02-04 14:02:10 +0100

    Initialisation du projet

---
+++ src/test/java/org/carbonit/treasurehunt/TreasureHuntTest.java
@@ -0,0 +1,13 @@
+package org.carbonit.treasurehunt;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+ class TreasureHuntTest {
+
+    @Test
+     void initTest(){
+        Assertions.assertTrue(true);
+    }
+}
